3460a5aa071e7b12bb85097ed7b4341f9e319b4b
nishigaya <c0b23111c6@edu.teu.ac.jp>
2024-11-26 14:34:40 +0900

    練習２：爆弾作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import os
+import random
 import sys
 import pygame as pg
-
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -20,11 +20,19 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20)) # 爆弾用の空Surface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) # 爆弾円を描く
+    bb_img.set_colorkey((0, 0, 0)) # 爆弾の四隅透過
+    bb_rct = kk_img.get_rect() # 爆弾Rectの抽出
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    # 右のコードでもいい bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5 # 爆弾速度ベクトル
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
         screen.blit(bg_img, [0, 0]) 
 
@@ -36,6 +44,8 @@ def main():
                 sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  # 爆弾動く
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
